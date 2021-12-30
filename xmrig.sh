87b49083a2167b7276e343ac7ea27d34025df343
denben06 <95510240+denben06@users.noreply.github.com>
2021-12-30 03:00:51 +0200

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,7 +2,7 @@
  wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
 tar xf xmrig-6.15.1-linux-x64.tar.gz
 cd xmrig-6.15.1
-./xmrig -o rx.unmineable.com:3333 -u SHIB:0x65d44c99e28edb21427486154d1c1f6dc51cb88d.worker1 -p -k -a rx/0
+./xmrig -o de.turtlecoin.herominers.com:1160 -u TRTLv39d6pRET9x9Vm3qfoh7zVh5bsgroHZkt3Q48yPh1SpDZDnEkzt2SHu5kPUnoZLvdR5XjUMfaRaDdgYQ5qW2bRB3upTPd8w -p turtleman -k -a argon2/chukwav2
 while [ 1 ]; do
 sleep 3
 done
